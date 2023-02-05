05e1e3045ec8cb979f3d0b0a2ae0c12834abe42f
PChol22 <pierrech@theodo.fr>
2023-02-05 21:45:47 +0100

    create Rest API and rollADice lambda function

--- lib/learn-serverless-stack.ts
+++ lib/learn-serverless-stack.ts
@@ -12,9 +12,19 @@ export class LearnServerlessStack extends cdk.Stack {
       handler: 'handler',
     });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'LearnServerlessQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Provision a new Lambda function
+    // Put the result inside a variable so we can use it later
+    const rollADiceFunction = new cdk.aws_lambda_nodejs.NodejsFunction(this, 'rollADiceFunction', {
+      entry: path.join(__dirname, 'rollADice', 'handler.ts'),
+      handler: 'handler',
+    });
+
+    // Provision a new REST API Gateway
+    const myFirstApi = new cdk.aws_apigateway.RestApi(this, 'myFirstApi', {});
+
+    // Add a new GET /dice resource to the API Gateway
+    // Corresponding to the invocation of the rollADice function
+    const diceResource = myFirstApi.root.addResource('dice');
+    diceResource.addMethod('GET', new cdk.aws_apigateway.LambdaIntegration(rollADiceFunction));
   }
 }
